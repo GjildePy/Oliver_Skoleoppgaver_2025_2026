6db0df1794e6accc7bf3bf2095d50cb3a2843c7a
GjildePy <gildeoliver07@gmail.com>
2025-09-11 10:35:57 +0200

    Ferdig kommentarer

--- Main.py
+++ Main.py
@@ -1,11 +1,12 @@
+#nominert_1/2 er en variabel som blir lagd
 nominert_1=input("skriv inn navnet til første nominert: ")
 nominert_2=input("skriv inn navnet til andre nominert: ")
 
-#Variabeler for hvem av de nominerte du kan stemme på
+# Variabeler for hvem av de nominerte du kan stemme på
 nom_1_votes=0
 nom_2_votes=0
 
-#en liste med "ID" som du bruker når du stemmer, så det er da 10 stykker som kan stemme
+# en liste med "ID" som du bruker når du stemmer, så det er da 10 stykker som kan stemme
 votes_id = [1,2,3,4,5,6,7,8,9,10]
 
 num_of_voter=len(votes_id)
@@ -13,24 +14,27 @@ num_of_voter=len(votes_id)
 while True:
     if votes_id == []:
         print("Avstemningssesjonen er over")
-        if nom_1_votes>nom_2_votes:
-            prosent = (nom_1_votes / num_of_voter) * 100
+        if nom_1_votes>nom_2_votes: # Hvis 1. nominerte har større verdi enn 2. nominerte printer den at nominert_1 har vunnet valget
+            prosent = (nom_1_votes / num_of_voter) * 100 # Ganger det med 100 for å få prosent
             print(nominert_1,"har vunnet valget","med",prosent,"% stemmer")
             break
 
-        elif nom_1_votes<nom_2_votes:
+        elif nom_1_votes<nom_2_votes: # Det samme som den over bare med motsatt nominert
             prosent = (nom_2_votes / num_of_voter) * 100
             print(nominert_2,"har vunnet valget","med",prosent,"% stemmer")
             break
 
-    else:
-        voter=int(input("Skriv inn velger-ID-en din ")) #Her skriver du "IDen" din og hvis du ikke skriver inn en gyldig "ID" eller en ID som allerede er brukt får du ikke stemmet.
-        if voter in votes_id:
-            print("Du er en velger ")
-            votes_id.remove(voter)
-            vote = int(input("Legg inn din stemme, 1 eller 2: ")) #Her velger du hvem av de to nominerte di stemmer
+        else:
+            print("Valget ble uavgjort!")
 
-            if vote==1:
+    else:
+        voter=int(input("Skriv inn velger-ID-en din ")) # Her skriver du "IDen" din og hvis du ikke skriver inn en gyldig "ID" eller en ID som allerede er brukt får du ikke stemmet.
+        if voter in votes_id: # Hvis "IDen" er gyldig printer den at du kan stemme og spør deg hva du vil stemme 
+            print("Du er en velger ")
+            votes_id.remove(voter) #Fjerner "IDen" som ble brukt så den ikke kan bli brukt på nytt
+            vote = int(input("Legg inn din stemme, 1 eller 2: ")) # Her velger du hvem av de to nominerte di stemmer
+
+            if vote==1: # Hvis stemme går til 1, får 1. nominerte ett poeng
                 nom_1_votes+=1
                 print("Takk for at du avga din stemme")
 
@@ -38,5 +42,5 @@ while True:
                 nom_2_votes+=1
                 print("Takk for at du avga din stemme")
     
-            else:
+            else: # Hvis brukeren skriver inn noe annet enn 1 eller 2
                 print("Du er ikke velger, eller så har du allerede stemt")
